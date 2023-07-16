a4ab6423ad940d265574fa6780323c5d3e9ff58e
Duncan Bennett <duncanb.dev@gmail.com>
2023-07-16 15:49:01 +0200

    Adds missing config files. Fixes broken js path.

--- src/routes/channel.js
+++ src/routes/channel.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET channels listing. */
-router.get('/', function(req, res, next) {
-  res.render('channels', { title: 'The Only Channel' });
+/* GET channel. */
+router.get('/:user_id', function(req, res, next) {
+  res.render('channel', { title: 'The Only Channel' });
 });
 
 module.exports = router;
